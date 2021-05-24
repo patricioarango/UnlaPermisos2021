f3381844ad87a1d52fb74715081e774ceeb6ebf7
patricioarango <patricioarango@gmail.com>
2021-05-24 19:02:24 -0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# UnLa Certificados - OO2 2021
+Proyecto 2021 Springboot 
+
+## Patricio Arango | Walter Altamiranda | Luis Alegre 
+
+## Instalación
+El proyecto funciona en http://localhost:9090 ya que el puerto 8080 lo tenía tomado en mi compu.
+
+## Usuario por defecto
+```bash
+administrador
+admin123
+```
---
+++ src/main/java/com/unlapermisos2021/UnlaPermisos2021Application.java
@@ -0,0 +1,13 @@
+package com.unlapermisos2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnlaPermisos2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnlaPermisos2021Application.class, args);
+	}
+
+}
