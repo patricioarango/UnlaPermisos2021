7e030fe9f83946c9d17ef1818e18b626c26524b7
patricioarango <patricioarango@gmail.com>
2021-05-25 23:23:04 -0300

    varios

--- src/main/java/com/unlapermisos2021/controllers/UsuariosController.java
+++ src/main/java/com/unlapermisos2021/controllers/UsuariosController.java
@@ -1,5 +1,7 @@
 package com.unlapermisos2021.controllers;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 import com.unlapermisos2021.converters.UsuarioConverter;
 import com.unlapermisos2021.entities.Usuario;
@@ -25,15 +26,19 @@ public class UsuariosController {
 	private UsuarioConverter userConverter;
    
 	@GetMapping("/listar")
-	public ModelAndView listar(Model model) {
+	public ModelAndView listar(Model model,HttpSession session) {
+		session.setAttribute("SessionRol", "ADMINISTRADOR");
 		ModelAndView mav = new ModelAndView(ViewRoutesHelper.USUARIOS_LISTADO);
 		model.addAttribute("usuarios", userService.getAllEnabled(1));
         return mav;
 	}
 	
 	@GetMapping("/nuevo")
-	public String nuevo() {
-		return "usuarios/form_usuario";
+	public ModelAndView nuevo(Model model) {
+		ModelAndView mav = new ModelAndView(ViewRoutesHelper.USUARIOS_FORM);
+        UsuarioModel userModel =  new UsuarioModel();
+        mav.addObject("usuario", userModel);
+        return mav;
 	}
 	
 	@GetMapping("/modificar/{id}")
